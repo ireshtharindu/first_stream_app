5ad3cd84a4110577f4b3a754741662f560f01b27
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-29 14:03:36 +0530

    Update Steamlit_app.py
    Add Breakfast List

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
 streamlit.title('My Pairents New Healthy Diner') 
+
+streamlit.header('Breakfast Menu') 
+streamlit.text('Omega 3 & blueberry Oatmeal') 
+streamlit.text('Kale, spanish & Rocket Smoothie') 
+streamlit.text('Hard-Boiled Free-Range Egg') 
+
+
