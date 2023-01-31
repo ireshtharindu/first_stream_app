61c849b328d84129ebd20029a1b86c6c7714b878
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-31 08:36:53 +0530

    Update Steamlit_app.py
    add new selection

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -26,8 +26,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+#new selectionto response fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 #new selecetion to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
