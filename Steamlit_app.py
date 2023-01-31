10bc964662e576eebcfd3f0b6c79f90883683aa8
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-31 17:37:37 +0530

    Update Steamlit_app.py
    err fix

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -71,7 +71,7 @@ def insert_row_snowflake(new_fruit):
                return my_cur.fetchall()
         
 #add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Add  a Fruit to the list'):
+if streamlit.button('get_fruit_load_list'):
         my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
         my_data_rows = get_fruit_load_list()
         streamlit.dataframe(my_data_rows)
