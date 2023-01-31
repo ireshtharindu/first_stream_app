9325a508223bd4f9bfdaf2dd5bc86d623626ff2b
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-31 16:43:40 +0530

    Update Steamlit_app.py
    make changes big edit

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -33,19 +33,21 @@ streamlit.dataframe(fruits_to_show)
 
 #new selectionto response fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-#new selecetion to display fruityvice api response
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? - Take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?- Out put it and screen the table
-
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+    else:
+        #new selecetion to display fruityvice api response
+        #import requests
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        #streamlit.text(fruityvice_response.json())
+        # write your own comment -what does the next line do? - Take the json version of the response and normalize it
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        # write your own comment - what does this do?- Out put it and screen the table
+        streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #Do not run anything past hearwhile we troubleshoot
 streamlit.stop()
