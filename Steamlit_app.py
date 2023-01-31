4d845ba5f47923944cf58a6980f9d471b5afdff1
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-31 16:45:42 +0530

    Update Steamlit_app.py
    error change

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -37,7 +37,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
-    else:
+  else:
         #new selecetion to display fruityvice api response
         #import requests
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
