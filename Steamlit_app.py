71fff2650f62c41229e7a486fd4c16fdfd0f9820
Iresh Tharindu <68737844+ireshtharindu@users.noreply.github.com>
2023-01-31 16:22:50 +0530

    Update Steamlit_app.py
    lesson 12 tutorial

--- Steamlit_app.py
+++ Steamlit_app.py
@@ -57,4 +57,8 @@ streamlit.dataframe(my_data_rows)
 #new selectionto response fruityvice api response
 #streamlit.header("Fruityvice Fruit Advice!")
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#lesson12
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
